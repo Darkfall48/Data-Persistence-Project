c386ba33e6c03d1aed661716bd5bfae494001bf3
Darkfall48 <sidney@sebban.be>
2022-10-15 03:42:36 +0300

    Start and Exit Button
    - Added a Start button
    - Added an Exit button

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -12,20 +12,20 @@ public class MainManager : MonoBehaviour
 
     public Text ScoreText;
     public GameObject GameOverText;
-    
+
     private bool m_Started = false;
     private int m_Points;
-    
+
     private bool m_GameOver = false;
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
@@ -73,4 +73,5 @@ public class MainManager : MonoBehaviour
         m_GameOver = true;
         GameOverText.SetActive(true);
     }
+
 }
---
+++ Assets/Scripts/MenuUI.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MenuUI : MonoBehaviour
+{
+    public void StartNew()
+    {
+        // Load Main Scene
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+
+        // Les '#' servent a garder le code qui est vrai (ou faux), l'autre ne sera pas pris en compte
+        #if (UNITY_EDITOR)
+        {
+            // Quit the game (Unity Editor)
+            EditorApplication.ExitPlaymode();
+        }
+
+        #else
+        {
+            // Quit the game (Game Build)
+            Application.Quit();
+        }
+        
+        #endif
+
+    }
+}
